bc39d75c2c537f93b5fd7d246bb9054138fc7fbb
Sakar <techiesakar@gmail.com>
2022-01-30 18:49:36 +0545

    Day 6 Started

---
+++ Functions/main.js
@@ -0,0 +1,17 @@
+function logger() {
+    console.log(`My name is Sakar`)
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges)
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice
+}
+
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice)
+
+const appleOrangeJuice = fruitProcessor(2, 4)
+console.log(appleOrangeJuice)
